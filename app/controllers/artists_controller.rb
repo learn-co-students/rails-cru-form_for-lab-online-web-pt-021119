df98df3adaa959636797bc0af3856fd1466931d5
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-07-06 14:47:08 -0500

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,32 @@
+class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.find(params[:id])
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def create
+    @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist = Artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect_to artists_path
+  end
+
+end
