39e3ed19c26dc4f556579510aca62c1a7956e9b1
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-07-07 13:49:42 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,36 @@
 class SongsController < ApplicationController
+
+  def new
+    @song = Song.new
+  end
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.create(params.require(:song).permit(:name))
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(params.require(:song).permit(:name))
+    redirect_to song_path(@song)
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to songs_path
+  end
+
 end
